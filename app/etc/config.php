e8a9140b501d7cb3c8e91af067b8168576908967
Serj <serhii.lazebnyi@gmail.com>
2020-09-06 15:52:00 +0300

    Homework #1 Soft Dependency

--- app/etc/config.php
+++ app/etc/config.php
@@ -296,7 +296,7 @@ return [
         'PayPal_Braintree' => 1,
         'PayPal_BraintreeGraphQl' => 1,
         'Temando_ShippingRemover' => 1,
-        'Training_Dependency2' => 1,
+        'Training_Dependency2' => 0,
         'Training_Dependency' => 1,
         'Vertex_Tax' => 1,
         'Vertex_AddressValidation' => 1,
