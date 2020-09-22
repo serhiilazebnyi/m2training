af87844136300de24fe45b6773a3b59d601ae64e
Serj <serhii.lazebnyi@gmail.com>
2020-09-22 13:35:18 +0300

    Homework #1.5 di.xml part 4 - factory for interface

--- app/code/Training/TestOM/Model/Test.php
+++ app/code/Training/TestOM/Model/Test.php
@@ -8,27 +8,33 @@ class Test
     private $arrayList;
     private $name;
     private $number;
+    private $managerFactory;
 
     public function __construct(
         ManagerInterface $manager,
         $name,
         int $number,
-        array $arrayList
+        array $arrayList,
+        ManagerInterfaceFactory $managerFactory //generated/code/Training/TestOM/Model/ManagerInterfaceFactory.php
     ) {
         $this->manager = $manager;
         $this->name = $name;
         $this->number = $number;
         $this->arrayList = $arrayList;
+        $this->managerFactory = $managerFactory;
     }
 
     public function log()
     {
-        print_r(get_class($this->manager));
+        print_r(get_class($this->manager)); //Training\AdditionalTestOM\Model\ManagerCustom
         echo '<br>';
         print_r($this->name);
         echo '<br>';
         print_r($this->number);
         echo '<br>';
         print_r($this->arrayList);
+        echo '<br>';
+        $newManager = $this->managerFactory->create();
+        print_r(get_class($newManager)); //Training\AdditionalTestOM\Model\ManagerCustom
     }
 }
