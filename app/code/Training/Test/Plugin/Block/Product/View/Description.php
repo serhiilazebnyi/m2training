f5397d8e81fb36f299042c0ad7a0d8c4992dbd04
Serhii Lazebnyi <s.lazebny@dinarys.com>
2020-10-25 22:41:47 +0200

    3.7 - Replace product description block

--- app/code/Training/Test/Plugin/Block/Product/View/Description.php
+++ app/code/Training/Test/Plugin/Block/Product/View/Description.php
@@ -7,6 +7,8 @@ class Description
     public function beforeToHtml(
         \Magento\Catalog\Block\Product\View\Description $subject
     ) {
-        $subject->getProduct()->setDescription('Test description');
+        if ($subject->getNameInLayout() == 'product.info.description') {
+            $subject->setTemplate('Training_Test::description.phtml');
+        }
     }
 }
