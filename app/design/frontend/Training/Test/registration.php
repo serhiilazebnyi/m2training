c968faf2d6306f8e7023280a1f0b3d4a45c6f3c4
Serhii Lazebnyi <s.lazebny@dinarys.com>
2020-10-26 01:03:38 +0200

    3.15 - Register new theme

---
+++ app/design/frontend/Training/Test/registration.php
@@ -0,0 +1,9 @@
+<?php
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::THEME,
+    'frontend/Training/test',
+    __DIR__
+);
